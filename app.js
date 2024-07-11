007d7d8bfc63f97ec1876b46c088ab51fc105f38
Dayanand <dayukori@gmail.com>
2024-07-11 22:52:37 +0530

    Work under Progress

---
+++ app.js
@@ -0,0 +1,110 @@
+let Num1=null;
+let Num2=null;
+let displayValue="";
+let operator="";
+
+function updateDisplay(value) {
+    displayValue += value;
+    document.querySelector('.displayScreen').textContent = displayValue;
+}
+
+document.querySelectorAll('.number').forEach(item => {
+    item.addEventListener('click', event => {
+        updateDisplay(event.target.textContent);
+    });
+});
+
+document.querySelectorAll('.operator').forEach(item=> {
+    item.addEventListener('click', event => {
+        const clickedOperator=event.target.textContent;
+
+        if(clickedOperator==='C'){
+            clear();
+        }
+        else if(clickedOperator==='⌫'){
+            backSpace();
+        }
+        else if (clickedOperator === '=') {
+            if (operator !== "" && Num1 !== null && displayValue !== "") {
+                Num2 = parseFloat(displayValue);
+                const result = operations(Num1, Num2, operator);
+                displayValue = result.toString();
+                document.querySelector('.displayScreen').textContent = displayValue;
+                Num1 = result;
+                Num2 = null;
+                displayValue = "";
+            }
+        }
+        else {
+            if (operator !== "" && Num1 !== null && displayValue !== "") {
+                Num2 = parseFloat(displayValue);
+                const result = operations(Num1, Num2, operator);
+                displayValue = result.toString();
+                document.querySelector('.displayScreen').textContent = displayValue;
+                Num1 = result;
+                Num2 = null;
+                displayValue = "";
+            }
+
+            Num1 = parseFloat(displayValue);
+            operator = clickedOperator;
+            displayValue = "";
+        }
+    });
+});
+
+function add(a,b){
+    return a+b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function multiply(a, b){
+    return a*b;
+}
+
+function divide(a,b){
+    if(b!==0){
+        return a/b;
+    }
+    else{
+        return "undefined";
+    }
+}
+
+function inversion(a){
+    return -1*a;
+}
+
+function clear(){
+    Num1='';
+    Num2='';
+    operator=''
+    displayValue='';
+    document.querySelector('.displayScreen').textContent = displayValue;
+}
+
+function backSpace(){
+    if(displayValue!==''){
+        displayValue='';
+    }
+    document.querySelector('.displayScreen').textContent = displayValue;
+}
+
+function operations(Num1, Num2, operator) {
+    switch (operator) {
+        case '+':
+            return add(Num1, Num2);
+        case '-':
+            return subtract(Num1, Num2);
+        case 'X':
+            return multiply(Num1, Num2);
+        case '÷':
+            return divide(Num1, Num2);
+        default:
+            return Num2;
+    }
+}
+
